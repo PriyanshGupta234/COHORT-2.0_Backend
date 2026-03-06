0845160fee616e5f5cfcfbc94ec6d33c170bf7de
PriyanshGupta234 <priyanshguptaji234@gmail.com>
2026-03-06 11:24:52 +0530

    Loading state

--- day-20/Frontend/src/features/auth/pages/Login.jsx
+++ day-20/Frontend/src/features/auth/pages/Login.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import "../styles/form.scss";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const { handleLogin } = useAuth();
+  const { handleLogin, loading } = useAuth();
+  const navigate = useNavigate();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
 
     handleLogin(username, password).then((res) => {
       console.log(res);
+      navigate("/");
     });
   }
 
