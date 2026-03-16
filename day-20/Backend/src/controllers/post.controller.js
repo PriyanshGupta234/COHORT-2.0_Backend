6a29c34a76e51a6500e0108e7f2ee539b186850f
PriyanshGupta234 <priyanshguptaji234@gmail.com>
2026-03-16 23:15:20 +0530

    like and unlike

--- day-20/Backend/src/controllers/post.controller.js
+++ day-20/Backend/src/controllers/post.controller.js
@@ -99,11 +99,38 @@ async function likePostController(req, res) {
     })
 }
 
+async function unLikePostController(req, res) {
+
+    const postId = req.params.postId;
+    const username = req.user.username;
+
+    const isLiked = await likeModel.findOne({
+        post: postId,
+        user: username
+    })
+
+    if (!isLiked) {
+        return res.status(400).json({
+            message: "Post didn't like"
+        })
+    }
+
+    await likeModel.findOneAndDelete({
+        _id: isLiked._id
+    })
+
+    return res.status(200).json({
+        message: "post unliked successfully."
+    })
+}
+
 async function getFeedController(req, res) {
 
     const user = req.user
 
-    const posts = await Promise.all((await postModel.find().populate("user").lean())
+    const posts = await Promise.all((await postModel.find({}).sort({
+            _id: -1
+        }).populate("user").lean())
         .map(async (post) => {
 
             const isLiked = await likeModel.findOne({
@@ -126,5 +153,6 @@ module.exports = {
     getPostController,
     getPostDetailsController,
     likePostController,
-    getFeedController
+    getFeedController,
+    unLikePostController
 }
--- day-20/Frontend/src/features/posts/hook/usePost.js
+++ day-20/Frontend/src/features/posts/hook/usePost.js
@@ -1,8 +1,12 @@
 import {
-    getFeed
+    getFeed,
+    createPost,
+    likePost,
+    unLikePost
 } from "../services/post.api"
 import {
-    useContext
+    useContext,
+    useEffect
 } from "react"
 import {
     PostContext
@@ -24,14 +28,39 @@ export const usePost = () => {
     const handleGetFeed = async () => {
         setLoading(true)
         const data = await getFeed()
-        setFeed(data.posts)
+        setFeed(data.posts.reverse())
         setLoading(false)
     }
+
+    const handleCreatePost = async (imageFile, caption) => {
+        setLoading(true)
+        const data = await createPost(imageFile, caption)
+        setFeed(prev => [data.post, ...feed])
+        setLoading(false)
+    }
+
+    const handleLike = async (post) => {
+        const data = await likePost(post)
+        await handleGetFeed()
+    }
+
+    const handleUnLike = async (post) => {
+        const data = await unLikePost(post)
+        await handleGetFeed()
+    }
+
+
+    useEffect(() => {
+        handleGetFeed()
+    }, [])
 
     return {
         loading,
         feed,
         post,
-        handleGetFeed
+        handleGetFeed,
+        handleCreatePost,
+        handleLike,
+        handleUnLike
     }
 }
